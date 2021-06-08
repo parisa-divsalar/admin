d58f7332b3830423b2a63cdf4142377bf1256198
qhazale <g.bakhtiari@tourismit.ir>
2021-06-08 09:40:22 +0430

    Add a initialization

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +10,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+    int a;
 
 }
